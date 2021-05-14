c8a0ae91a652d2b0ec523c076cd7dc15f612d3f8
sanpidang <yebo79@gmail.com>
2021-05-14 18:46:07 +1000

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'http://localhost:8081/',
+  json: true
+})
+
+export default {
+  async execute (method, resource, data) {
+    // inject the accessToken for each request
+    return client({
+      method,
+      url: resource,
+      data
+    }).then(req => {
+      return req.data
+    })
+  },
+  getPosts () {
+    return this.execute('get', '/posts')
+  },
+  getPost (id) {
+    return this.execute('get', `/posts/${id}`)
+  },
+  createPost (data) {
+    return this.execute('post', '/posts', data)
+  },
+  updatePost (id, data) {
+    return this.execute('put', `/posts/${id}`, data)
+  },
+  deletePost (id) {
+    return this.execute('delete', `/posts/${id}`)
+  }
+}
